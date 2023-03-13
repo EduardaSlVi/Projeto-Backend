dff9b3df6b32191751f002ac88bc1bb3a6f13303
Eduarda Vieira <106997851+EduardaSlVi@users.noreply.github.com>
2023-03-13 16:32:02 -0300

    Add files via upload

---
+++ pessoa/pessoa/src/main/java/br/com/aulabe/pessoa/Pessoa.java
@@ -0,0 +1,5 @@
+package br.com.aulabe.agenda;
+
+public class Pessoa {
+
+}
---
+++ pessoa/pessoa/src/main/java/br/com/aulabe/pessoa/PessoaController.java
@@ -0,0 +1,42 @@
+package br.com.aulabe.agenda;
+
+
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.http.*;
+
+@RestController
+@RequestMapping(value = "/pessoa")
+public class PessoaController {
+
+    @Autowired
+    private PessoaService pessoaService;
+
+    @GetMapping
+    public ResponseEntity<List<Pessoa>> listarPessoas() {
+        List<Pessoa> pessoas = pessoaService.listarPessoas();
+        return ResponseEntity.ok(pessoas);
+    }
+
+    @PostMapping
+    public ResponseEntity<Pessoa> criarPessoa(@RequestBody Pessoa pessoa) {
+        Pessoa novaPessoa = pessoaService.criarPessoa(pessoa);
+        return ResponseEntity.status(HttpStatus.CREATED).body(novaPessoa);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deletarPessoa(@PathVariable Long id) {
+        pessoaService.deletarPessoa(id);
+        return ResponseEntity.noContent().build();
+    }
+}
+
